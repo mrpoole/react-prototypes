8eae64a3887bcaf66cd54c5bf95b0d2e839f8cf6
Michelle Poole <michelle.poole.email@gmail.com>
2019-03-27 14:18:18 -0700

    completed functional components

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React from 'react';
+import Table from './table';
 
+export default () => {
 
+    const students = [
+        {
+            name: 'Jim Bob',
+            course: 'Whittling',
+            grade: 100
+        },
+        {
+            name: 'Boba Fett',
+            course: 'Bounty Hunting',
+            grade: 100
+        },
+        {
+            name: 'Lando Calrissian',
+            course: 'Sales',
+            grade: 100
+        }
+    ];
+
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
