13de8d1338d51b3b940b1e284304e65e70fa3f84
Michelle Poole <michelle.poole.email@gmail.com>
2019-07-24 16:27:47 -0700

    completed the intro to react prototype

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,30 @@
+import React, { Component } from 'react'
 
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.user = {
+            name: 'Bob',
+            number: this.luckyNumber()
+        }
+    }
 
+    luckyNumber = () => {
+        return Math.floor(Math.random() * 1001);
+    }
+
+    greeting(user){
+        return(
+            <div className="container">
+                <h1>Hello {user.name}!</h1>
+                <h2 className="text-muted">You are lucky number {user.number}</h2>
+            </div>
+        )
+    }
+
+    render() {
+        return (   
+            this.greeting(this.user)
+        )
+    }
+}
